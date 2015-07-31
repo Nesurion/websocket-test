3e06f55fe8016277bd0bc93e703ee29d8e1baeba
Nesurion <maik.schwan@gmail.com>
2015-07-31 10:48:43 +0200

    more verbose logging

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM ruby:2.1-onbuild
 MAINTAINER Maik Schwan <maik.schwan@gmail.com>
 
-EXPOSE 4567
+EXPOSE 80
 CMD ["ruby", "./socker.rb"]
--- socker.rb
+++ socker.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
 require 'sinatra-websocket'
+require 'logger'
 
 set :server, 'thin'
 set :sockets, []
 set :bind, "0.0.0.0"
+set :port, 80
+
+logger = Logger.new(STDOUT)
+logger.level = Logger::INFO
+
+api_key = 'FAA7A16539B96341D2BB81F724F43'
 
 get '/' do
   if !request.websocket?
@@ -13,13 +20,14 @@ get '/' do
       ws.onopen do
         ws.send("Hello World!")
         settings.sockets << ws
-        puts "new socket opened"
+        logger.info("new socket opened: #{ws}")
       end
       ws.onmessage do |msg|
+        logger.info("message: #{msg} [socket: #{ws}]")
         EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
       end
       ws.onclose do
-        warn("websocket closed")
+        logger.info("websocket #{ws} closed")
         settings.sockets.delete(ws)
       end
     end
@@ -27,15 +35,23 @@ get '/' do
 end
 
 get '/api/sockets/close' do
-  settings.sockets.each do |socket|
-    socket.close_connection(1000)
-  end
-  return "closed all sockets"
+  # if headers["X-AUTH"] == api_key
+    settings.sockets.each do |socket|
+      socket.close_connection(1000)
+    end
+    return "closed all sockets"
+  # else
+  #   status 403
+  # end
 end
 
 get '/api/sockets/list' do
-  settings.sockets.each do |socket|
-    puts socket
-  end
-  return "logged sockets"
+  # if headers["X-AUTH"] == api_key
+    settings.sockets.each do |socket|
+      puts socket
+    end
+    return "logged sockets"
+  # else
+  #   status 403
+  # end
 end
